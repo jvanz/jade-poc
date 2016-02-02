969f15e1f200d0010b569a0d2104a00f6896ed6a
José Guilherme Vanz <guilherme.sft@gmail.com>
2016-02-02 00:39:51 -0200

    templates: add initial template for pages
    Add base templates should be used to create web pages

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const PORT = 8080;
 var index = jade.compileFile("template/index.jade");
 
 function handle_request(request, response){
-	response.end(index());
+	console.log("Hit path:", request.url);
+	headers = ["users", "users","users","users","users","users"];
+	response.end(index({headers: headers}));
 }
 
 var server = http.createServer(handle_request);
