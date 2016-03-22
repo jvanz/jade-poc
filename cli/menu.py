083720a3d7c93c414852ff5131b4010589f64f54
José Guilherme Vanz <guilherme.sft@gmail.com>
2016-03-21 22:04:08 -0300

    cli: menu.py
    Adds file with the code to handle menu in CLI

---
+++ cli/menu.py
@@ -0,0 +1,65 @@
+import curses
+from curses  import panel
+from api import get_user, get_device, delete_user, delete_device
+
+class Menu:
+	"""Class to handle the CLI menu"""
+
+	def __init__(self, screen, y, x):
+		self.__screen = screen
+		self.win = curses.newwin(int(y), int(x), 0, 0)
+		self.win.box()
+		self.__options = [MenuItem("User", get_user, delete_user),
+				MenuItem("Devices", get_device, delete_device)]
+		self.__current_focus = 0
+		self.panel = panel.new_panel(self.win)
+		self.panel.set_userptr(self)
+		self.select()
+
+	def draw(self):
+		index = 1
+		for opt in self.__options:
+			if opt.focus:
+				self.win.addstr(index, 1, opt.text,
+						curses.color_pair(1))
+			else:
+				self.win.addstr(index, 1, opt.text)
+			index += 1
+
+	def show(self):
+		self.panel.top()
+		self.panel.show()
+
+	def hide(self):
+		self.panel.hide()
+
+	def select(self, index=0):
+		if index > len(self.__options) - 1:
+			index = 0
+		self.__options[self.__current_focus].focus = False
+		self.__options[index].focus = True
+		self.__current_focus = index
+
+
+	def input_key(self, key):
+		"""Handles input keys while menu is the top panel in screen"""
+		if key is None:
+			return
+		if curses.KEY_UP == key:
+			self.select(self.__current_focus+1)
+		elif curses.KEY_DOWN == key:
+			self.select(self.__current_focus-1)
+		elif curses.KEY_ENTER == key or 10 == key or 13 == key:
+			option = self.__options[self.__current_focus]
+			self.__screen.show_table(option.data_function,
+					option.remove_function)
+
+class MenuItem:
+	"""Class to store menu items data"""
+
+	def __init__(self, text, fetch_data_function, remove_function):
+		self.text = text
+		self.focus = False
+		self.data_function = fetch_data_function
+		self.remove_function = remove_function
+
